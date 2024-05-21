766ea65d433ed56f211bd41a4db24f11bcf943c5
Hermien van Staden <hermienvanstaden@gmail.com>
2024-05-21 22:07:59 +0200

    modified MyBand/settings.py to remove secret key

--- MyBand/MyBand/settings.py
+++ MyBand/MyBand/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8l1g(fzjd9$4o5=(3n((#_rxtw=912j*a7y$--ii1!2hq&beu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
